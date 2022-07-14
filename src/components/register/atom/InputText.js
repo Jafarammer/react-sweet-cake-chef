09af4a967ad193618c9c57e7a0be46bc9dda9105
Jafarammer <jafalkaff96@gmail.com>
2022-07-14 12:41:59 +0700

    register connect to backend

--- src/components/register/atom/InputText.js
+++ src/components/register/atom/InputText.js
@@ -8,17 +8,19 @@ const InputText = (props) => {
     label: labelInput,
     type: typeInput,
     placeholder: placeholderInput,
+    onChange: onChangeInput,
   } = props;
   return (
     <>
       {/* <Col> */}
       {/* <Form className="form-register"> */}
-      <Form.Group className={`mb-3 ${styles.form_group_register}`}>
+      <Form.Group className={`mb-3 mt-3 ${styles.form_group_register}`}>
         <Form.Label>{labelInput}</Form.Label>
         <Form.Control
           type={typeInput}
           className="py-3 px-4"
           placeholder={placeholderInput}
+          onChange={onChangeInput}
         />
       </Form.Group>
       {/* </Form> */}
--- src/components/register/organism/FormRegister.js
+++ src/components/register/organism/FormRegister.js
@@ -1,39 +1,86 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 // import atom
 import InputText from "../atom/InputText";
 //import css
 import styles from "../../../css/register.module.css";
+// axios
+import axios from "axios";
+// react router
+import { useNavigate } from "react-router-dom";
 
-const FormRegister = () => {
+function FormRegister() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [isError, setIsError] = React.useState("");
+  const history = useNavigate(); //route
+
+  const handleRegister = () => {
+    axios
+      .post("http://localhost:8000/users/add", {
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber,
+        password: password,
+        confirmPassword: confirmPassword,
+      })
+      .then((res) => {
+        setIsError(false);
+        window.location.href = "/login";
+      })
+      .catch((err) => {
+        setIsError(true);
+        setErrorMessage(err?.response?.data);
+      });
+  };
   return (
     <>
-      <Form className={styles.form_register}>
+      <Form
+        className={styles.form_register}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        {/* Error message */}
+        <Form.Group className={`mb-3 ${styles.form_group_register}`}>
+          {isError ? <Alert variant="danger">{errorMessage}</Alert> : null}
+        </Form.Group>
         {/* Name */}
-        <InputText label="Name" type="text" placeholder="Name" />
+        <InputText
+          label="Name"
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
         {/* Email */}
         <InputText
           label="Email address"
           type="email"
           placeholder="Enter email address"
+          onChange={(e) => setEmail(e.target.value)}
         />
         {/* Phone number */}
         <InputText
           label="Phone number"
           type="text"
           placeholder="08xxxxxxxxxxx"
+          onChange={(e) => setPhoneNumber(e.target.value)}
         />
         {/* Create password */}
         <InputText
           label="Create new password"
           type="password"
           placeholder="Create new password"
+          onChange={(e) => setPassword(e.target.value)}
         />
         {/* Confirm password */}
         <InputText
           label="Confirm password"
           type="password"
           placeholder="Confirm password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
         {/* Form check */}
         <Form.Group className={`mb-3 ${styles.form_group_register}`}>
@@ -50,12 +97,13 @@ const FormRegister = () => {
             type="submit"
             value="Register Account"
             className={`btn-warning text-white mt-3 py-3 px-4 ${styles.btn_register}`}
+            onClick={handleRegister}
           />
         </Form.Group>
 
         <p className="text-secondary">
           Already have account?
-          <a href="#" className="text-warning">
+          <a href="/login" className="text-warning">
             Log In Here
           </a>
         </p>
@@ -63,5 +111,5 @@ const FormRegister = () => {
       {/* End Form */}
     </>
   );
-};
+}
 export default FormRegister;
